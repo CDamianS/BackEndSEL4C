c41d97d4ee1354b08586e780193698b8de0b0c49
PabloCeb2000 <113377312+PabloCeb2000@users.noreply.github.com>
2023-09-21 16:47:52 -0600

    Migraciones y creación de base de datos

--- SEL4C/SEL4C/settings.py
+++ SEL4C/SEL4C/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'SEL4C.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'data',
+        'USER': 'datauser',
+        'PASSWORD': 'SEL4C',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
