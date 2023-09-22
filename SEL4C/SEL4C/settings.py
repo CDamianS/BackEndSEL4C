a647fe1a8c41e88a055871359eb11a7e2ae4664e
PabloCeb2000 <113377312+PabloCeb2000@users.noreply.github.com>
2023-09-21 22:31:20 -0600

    Configuraciones de REST y api

--- SEL4C/SEL4C/settings.py
+++ SEL4C/SEL4C/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-1+i0urx2%^)*tr+f(n@o96$d*h)t*n&k9xv2k%)u&#au$bjwtr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- SEL4C/SEL4C_Dashboard/urls.py
+++ SEL4C/SEL4C_Dashboard/urls.py
@@ -1,8 +1,12 @@
 from django.urls import include, path
-
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+
 urlpatterns = [
-    path('',views.index, name='index'),
-    
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    path('',views.index, name='index'), 
 ]
