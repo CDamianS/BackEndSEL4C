27c38727e0ec6150e81c4b1c4d6bc9c65b5bda66
CDamianS <A01656277@tec.mx>
2023-09-24 15:37:40 -0600

    Migrations, urls paths, cuestionario_inicial done

--- SEL4C/api/urls.py
+++ SEL4C/api/urls.py
@@ -4,4 +4,7 @@ from django.contrib.auth.decorators import login_required
 from .views import *
 
 urlpatterns = [
+    path('cuestionario_inicial', cuestionario_inicial),
+    # path('validar_usario', login_required(set_play_ended), name='set_play_ended'),
+    # path('register_usuario', login_required(add_student_to_game), name='add_student_to_game'),
 ]
