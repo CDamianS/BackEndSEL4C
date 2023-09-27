dba523ba1e529a1cb52782f8e3a6a4da8fadd4d4
CDamianS <A01656277@tec.mx>
2023-09-26 18:06:03 -0600

    Fixed warnings

--- SEL4C/SEL4C_Dashboard/models.py
+++ SEL4C/SEL4C_Dashboard/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Usuario(models.Model):
     usuarioID = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
@@ -9,14 +10,14 @@ class Usuario(models.Model):
     email = models.EmailField(max_length=200)
     avance = models.IntegerField()
     genero = models.CharField(max_length=20)
-    edad = models.IntegerField(max_length=5)
+    edad = models.IntegerField()
     pais = models.CharField(max_length=50)
     institucion = models.CharField(max_length=100)
     grado = models.CharField(max_length=50)
     diciplina = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'Usuario {self.usuarioID}: {self.nombre} - Email: {self.email} - Avance: {self.avance} - Genero: {self.genero} - Edad: {self.genero} - Pais: {self.pais} - Institucion: {self.institucion} - Grado: {self.grado} - Diciplina: {self.diciplina}'
+        return f"Usuario {self.usuarioID}: {self.nombre} - Email: {self.email} - Avance: {self.avance} - Genero: {self.genero} - Edad: {self.genero} - Pais: {self.pais} - Institucion: {self.institucion} - Grado: {self.grado} - Diciplina: {self.diciplina}"
 
 
 class Admin(models.Model):
@@ -24,33 +25,36 @@ class Admin(models.Model):
     nombre = models.CharField(max_length=50, unique=True)
     contrasenia = models.CharField(max_length=50)
 
-    def __str__ (self):
-        return f'Admin {self.adminID}: {self.nombre}'
-    
+    def __str__(self):
+        return f"Admin {self.adminID}: {self.nombre}"
+
+
 class Actividad(models.Model):
     actividadID = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     estatus = models.CharField(max_length=15)
-    usuarioID = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+    usuarioID = models.ForeignKey("Usuario", on_delete=models.CASCADE)
     entregable = models.CharField(max_length=7000)
 
     def __str__(self):
-        return f'Actividad {self.actividadID}: {self.nombre} - Estatus: {self.estatus} - Usuario: {self.usuarioID} - Entregable: {self.entregable}'
-    
+        return f"Actividad {self.actividadID}: {self.nombre} - Estatus: {self.estatus} - Usuario: {self.usuarioID} - Entregable: {self.entregable}"
+
+
 class CuestionarioInicial(models.Model):
     preguntaID = models.AutoField(primary_key=True)
     pregunta = models.CharField(max_length=100)
-    respuesta = models.CharField(max_length= 50)
-    usuarioID = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+    respuesta = models.CharField(max_length=50)
+    usuarioID = models.ForeignKey("Usuario", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Pregunra {self.preguntaID}: {self.pregunta} - Respuesta: {self.respuesta} - Usuario: {self.usuarioID}'
-    
+        return f"Pregunra {self.preguntaID}: {self.pregunta} - Respuesta: {self.respuesta} - Usuario: {self.usuarioID}"
+
+
 class CuestionarioFinal(models.Model):
     preguntaID = models.AutoField(primary_key=True)
     pregunta = models.CharField(max_length=100)
-    respuesta = models.CharField(max_length= 50)
-    usuarioID = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+    respuesta = models.CharField(max_length=50)
+    usuarioID = models.ForeignKey("Usuario", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Pregunra {self.preguntaID}: {self.pregunta} - Respuesta: {self.respuesta} - Usuario: {self.usuarioID}'
+        return f"Pregunra {self.preguntaID}: {self.pregunta} - Respuesta: {self.respuesta} - Usuario: {self.usuarioID}"
